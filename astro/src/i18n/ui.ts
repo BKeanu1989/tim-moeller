9265beb0c1750ace7e58570951076e4ccebbd34e
Kevin Fechner <bkevinfechner1989@gmail.com>
2024-10-23 17:58:31 +0200

    fix: navbar, projects & publ, i18n

--- astro/src/i18n/ui.ts
+++ astro/src/i18n/ui.ts
@@ -9,17 +9,27 @@ export const ui = {
     en: {
         'nav.home': 'Home',
         'nav.about': 'About me',
-        'nav.scientificProjects': 'Scientific Projects',
+        'nav.projects': 'Projects',
         'nav.publications': 'Publications',
         'nav.contact': 'Contact',
         'nav.shop': 'Shop',
+        'contact.name': 'Name',
+        'contact.email': 'Email',
+        'contact.subject': 'Subject',
+        'contact.send': 'Send',
+        'contact.message': 'Message'
     },
     de: {
         'nav.home': 'Home',
         'nav.about': 'Über mich',
-        'nav.scientificProjects': 'Wissenschaftliche Projekte',
-        'nav.publications': 'Veröffentlichungen',
+        'nav.projects': 'Projekte',
+        'nav.publications': 'Publikationen',
         'nav.contact': 'Kontakt',
         'nav.shop': 'Shop',
+        'contact.name': 'Name',
+        'contact.email': 'Email',
+        'contact.subject': 'Betreff',
+        'contact.send': 'Senden',
+        'contact.message': "Nachricht"
     },
 } as const;
