63f12288d3b821c575902b32f36b3eb7666f8ecd
Kevin Fechner <bkevinfechner1989@gmail.com>
2024-11-05 16:55:33 +0100

    chore: add custom colors, i18n

--- astro/tailwind.config.js
+++ astro/tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  // mode: "jit",
   content: ["./src/**/*.{html,js,astro}"],
   theme: {
     extend: {
       colors: {
-        neutral: "hsl(var(--neutral-cool-grey-1))",
-        "neutral-50": "hsl(var(--neutral-cool-grey-2))",
+        // neutral: "hsl(var(--neutral-cool-grey-1))",
+        // "neutral-50": "hsl(var(--neutral-cool-grey-2))",
+        "c-neutral-50": "hsl(var(--neutral-50)",
+        "c-neutral-100": "hsl(var(--neutral-100))",
+        "c-neutral-200": "hsl(var(--neutral-200))",
+        "c-neutral-300": "hsl(var(--neutral-300))",
+        "c-neutral-400": "hsl(var(--neutral-400))",
+        "c-neutral-500": "hsl(var(--neutral-500))",
+        "c-neutral-600": "hsl(var(--neutral-600))",
+        "c-neutral-700": "hsl(var(--neutral-700))",
+        "c-neutral-800": "hsl(var(--neutral-800))",
+        "c-neutral-900": "hsl(var(--neutral-900))",
+
+        // ""
       },
       fontFamily: {
         body: ['"Open Sans"'],
