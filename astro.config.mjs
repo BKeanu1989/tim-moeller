7edfa8f57201a1dcbd029c823a3b9568449c6174
Kevin Fechner <bkevinfechner1989@gmail.com>
2025-08-22 15:12:28 +0200

    fix: 'rechange' base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,33 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://bkeanu1989.github.io/",
-  base: "tim-moeller",
-  i18n: {
-    prefixDefaultLocale: true,
+	site: 'https://bkeanu1989.github.io/',
+	base: '/',
+	// base: "tim-moeller",
+	i18n: {
+		prefixDefaultLocale: true,
 
-    defaultLocale: "en",
-    locales: ["de", "en"],
-    // fallback: {
-    //   de: "en",
-    //   en: "de",
-    // },
-  },
-  // experimental: {
-  //   svg: true,
-  // },
+		defaultLocale: 'en',
+		locales: ['de', 'en']
+		// fallback: {
+		//   de: "en",
+		//   en: "de",
+		// },
+	},
+	// experimental: {
+	//   svg: true,
+	// },
 
-  integrations: [tailwind(), mdx()],
-  // image: {
-  //   service: "sharp",
-  //   serviceEntryPoint: "@astrojs/image/sharp",
-  //   logLevel: "info",
-  //   cacheDir: "./.cache/image",
-  //   defaultFormat: "webp",
-  // },
-});
+	integrations: [tailwind(), mdx()]
+	// image: {
+	//   service: "sharp",
+	//   serviceEntryPoint: "@astrojs/image/sharp",
+	//   logLevel: "info",
+	//   cacheDir: "./.cache/image",
+	//   defaultFormat: "webp",
+	// },
+})
